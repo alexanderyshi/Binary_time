18a07d2c2b09af1aed59c9f362cfd309dd6d9317
Alex Shi <alexander_shi@hotmail.com>
2016-06-08 01:55:56 -0400

    -added method for drawing pins -simplified graphics_update_proc

--- src/main.c
+++ src/main.c
@@ -14,51 +14,193 @@ static int debug_hide_time = -1;
 static char show_debug_time = 1; /* flag to allow debug time to show (see DESIGN:)*/
 static char show_weather_ic = 1;
 //callback constants
-#define KEY_SHOW_DEBUG_TIME 0
-#define KEY_SHOW_WEATHER 1
-#define KEY_TEMPERATURE 2
-#define IC_Colour ((uint8_t)0b11000001)
-#define Text_Colour ((uint8_t)0b11111111)
-#define MMDD_Light_Colour  ((uint8_t)0b11001101)
-#define Second_Light_Colour ((uint8_t)0b11010110)
-#define Min_Light_Colour ((uint8_t)0b11110010)
-#define Hour_Light_Colour ((uint8_t)0b11100011)
-#define Day_Light_Colour ((uint8_t)0b11000011)
-#define Month_Light_Colour ((uint8_t)0b11001001)
-#define Pin_Colour ((uint8_t)0b11101010)
-#define Night_Colour ((uint8_t)0b11000110)
-#define Dawn_Colour ((uint8_t)0b11100110)
-#define Day_Colour ((uint8_t)0b11001011)
-#define Noon_Colour ((uint8_t)0b11101111)
-#define SCREEN_HEIGHT 168
-#define SCREEN_WIDTH 144
-#define IC_WIDTH 40
-#define IC_HEIGHT 110
-#define IC_CORNER_RADIUS 2
-#define PIN_CORNER_RADIUS 2
-#define PIN_OFFSET 20
-#define PIN_WIDTH 20
-#define PIN_HEIGHT 10
-#define PIN_SCALE (3.0/4)
+#define KEY_SHOW_DEBUG_TIME     0
+#define KEY_SHOW_WEATHER        1
+#define KEY_TEMPERATURE         2
+//colours
+#define IC_Colour               ((uint8_t)0b11000001)
+#define Text_Colour             ((uint8_t)0b11111111)
+#define MMDD_Light_Colour       ((uint8_t)0b11001101)
+#define Second_Light_Colour     ((uint8_t)0b11010110)
+#define Min_Light_Colour        ((uint8_t)0b11110010)
+#define Hour_Light_Colour       ((uint8_t)0b11100011)
+#define Day_Light_Colour        ((uint8_t)0b11000011)
+#define Month_Light_Colour      ((uint8_t)0b11001001)
+#define Pin_Colour              ((uint8_t)0b11101010)
+#define Night_Colour            ((uint8_t)0b11000110)
+#define Dawn_Colour             ((uint8_t)0b11100110)
+#define Day_Colour              ((uint8_t)0b11001011)
+#define Noon_Colour             ((uint8_t)0b11101111)
+//dimensions
+#define SCREEN_HEIGHT           168
+#define SCREEN_WIDTH            144
+#define IC_WIDTH                40
+#define IC_HEIGHT               110
+#define IC_CORNER_RADIUS        2
+#define PIN_CORNER_RADIUS       2
+#define PIN_OFFSET              20
+#define PIN_WIDTH               20
+#define PIN_HEIGHT              10
+//scaling factors
+#define PIN_SCALE               (3.0/4) //used for seconds pins to shrink size
   
   
 
 //TODO: 
-//create config accessor .js file
-  //show debug time y/n
-  //static silverscreen style background or time of day colour
-  //include weather on shake
+
+//battery IC on top
+//static silverscreen style background or time of day colour
+//hide seconds config
+
+//weather on shake
 //daytime colour transition based on time of year from web sunrise/sunset time
-	//load an array with time offset values on setup + every time midnight hour is reached 
+  //load an array with time offset values on setup + every time midnight hour is reached 
 
 //DESIGN:
   //keep debug time as an easy to read feature for when the watch is prone to shake, i.e. running
+    //if it counts more than "x" shakes in "y" seconds, it will assume that I am running and enter a different type of mode
+
+//WATCHFACES AND APPS
+//make a cycling oriented watchface! 
+//make a cute weightlifting app for tracking PRs, with activity tracking
   
 
-	//function takes 2-char string and converts into an int
+  //function takes 2-char string and converts into an int
 static int int_from_string(char buffer[]) {
   return 10*(buffer[0]-'0') + (buffer[1]-'0');
 }
+
+static void _draw_vertical_pins(Layer *this_layer, GContext *ctx, int pins, int hor_coord, int vert_coord, char orientation, uint8_t colour, double scale){
+  //choose x position - by default draw pins above, if orientation is 'b' for below, draw below
+  int x_;
+  if (orientation == 'l') {
+     x_ = hor_coord - IC_WIDTH/2-PIN_WIDTH*scale;
+  }
+  else if (orientation == 'r'){
+     x_ = hor_coord + IC_WIDTH/2;
+  }
+  else{
+    return;
+  }
+  //draw pins
+  for (int i = 0; i<6; i++) {
+    int y_ = vert_coord-IC_HEIGHT/2+i*PIN_OFFSET;
+    if (pins >> (5-i) & 1)
+    {
+      graphics_context_set_fill_color(ctx, (GColor)colour);
+
+    }else{
+      graphics_context_set_fill_color(ctx, (GColor)Pin_Colour);
+    }
+    graphics_fill_rect(ctx, 
+               (GRect){.origin = (GPoint){.x = x_,.y = y_},
+               .size = (GSize){.w = PIN_WIDTH*scale,.h = PIN_HEIGHT}
+                  },
+               PIN_CORNER_RADIUS,
+               GCornersAll);
+
+  }// end for loop
+}
+
+static void _draw_horizontal_pins(Layer *this_layer, GContext *ctx, int pins, int hor_coord, int vert_coord, char orientation, uint8_t colour, double scale){
+  //choose y position - by default draw pins above, if orientation is 'b' for below, draw below
+  int y_;
+  if (orientation == 'a') {
+     y_ = vert_coord - IC_WIDTH/2-PIN_WIDTH*scale;
+  }
+  else if (orientation == 'b'){
+     y_ = vert_coord + IC_WIDTH/2;
+  }
+  else{
+    return;
+  }
+
+  //draw pins
+  for (int i = 0; i<6; i++) {
+    int x_ = hor_coord - IC_HEIGHT/2 + PIN_OFFSET*i;
+    if (pins >> (5-i) & 1)
+    {
+      graphics_context_set_fill_color(ctx, (GColor)colour);
+
+    }else{
+      graphics_context_set_fill_color(ctx, (GColor)Pin_Colour);
+    }
+    graphics_fill_rect(ctx, 
+               (GRect){.origin = (GPoint){.x = x_,.y = y_},
+               .size = (GSize){.w = PIN_HEIGHT,.h = PIN_WIDTH*scale}
+                  },
+               PIN_CORNER_RADIUS,
+               GCornersAll);
+
+  }// end for loop
+};
+
+static void draw_seconds_IC(Layer *this_layer, GContext *ctx, int hor_coord, int vert_coord) {
+  //rectangle gets drawn past the bottom limit of the screen to prevent the bottom from rounding
+  graphics_context_set_fill_color(ctx, (GColor)IC_Colour);
+  graphics_fill_rect(ctx, 
+                     (GRect){.origin = (GPoint){.x = hor_coord-IC_HEIGHT/2,.y = vert_coord-IC_WIDTH/2},
+                       .size = (GSize){.w = IC_HEIGHT,.h = IC_WIDTH}
+                            },
+                     IC_CORNER_RADIUS,
+                     GCornersAll);
+  //draw seconds pins
+  _draw_horizontal_pins(this_layer, ctx, num_second, hor_coord, vert_coord, 'a', Second_Light_Colour, PIN_SCALE);
+
+}
+
+static void draw_time_IC(Layer *this_layer, GContext *ctx, int hor_coord, int vert_coord){
+    //draw IC 
+    graphics_context_set_fill_color(ctx, (GColor)IC_Colour);
+    graphics_fill_rect(ctx, 
+          (GRect){.origin = (GPoint){.x = hor_coord-IC_WIDTH/2,.y = vert_coord-IC_HEIGHT/2},
+                .size = (GSize){.w = IC_WIDTH,.h = IC_HEIGHT}
+                },
+          IC_CORNER_RADIUS,
+          GCornersAll);
+           
+  //choose colours according to debug mode
+  int right_pins = debug_hide_time > 0 ? num_day : num_min;
+  uint8_t right_colour = debug_hide_time > 0 ? Day_Light_Colour : Min_Light_Colour;
+  int left_pins = debug_hide_time > 0 ? num_month : num_hour;
+  uint8_t left_colour = debug_hide_time > 0 ? Month_Light_Colour : Hour_Light_Colour;
+  uint8_t top_left_colour = debug_hide_time > 0 ? MMDD_Light_Colour : Second_Light_Colour;
+
+  _draw_vertical_pins(this_layer, ctx, right_pins, hor_coord, vert_coord, 'r', right_colour, 1);
+  _draw_vertical_pins(this_layer, ctx, left_pins, hor_coord, vert_coord, 'l', left_colour, 1);
+
+  graphics_context_set_fill_color(ctx, (GColor)top_left_colour);
+  //fill in remaining 6th pin
+  graphics_fill_rect(ctx, 
+          (GRect){.origin = (GPoint){.x = hor_coord-IC_WIDTH/2-PIN_WIDTH,.y = vert_coord-IC_HEIGHT/2},
+                .size = (GSize){.w = PIN_WIDTH,.h = PIN_HEIGHT}
+                },
+          IC_CORNER_RADIUS,
+          GCornersAll);
+}
+
+static void update_background_color(Layer *this_layer, GContext *ctx) {
+  //choose a background colour value once an hour in the tick handler, catching tick timer service subscription to updates in terms of minutes
+
+    //TODO: update background colour
+  //1300 treated as peak of day, sunrise/sunset is roughly 0600-2100 in summer
+  
+  //TODO: change time diff to account for minutes as well so that daylight hours can be more accurately tracked
+  unsigned int time_diff = (unsigned int)(num_hour-13>=0 ? num_hour-13 : 13-num_hour);
+  if (time_diff >= 8)
+  {
+    window_set_background_color(s_main_window, (GColor)Night_Colour);   
+  }else if (time_diff >= 6)
+  {
+    window_set_background_color(s_main_window, (GColor)Dawn_Colour);   
+  }else if (time_diff >= 2)
+  {
+    window_set_background_color(s_main_window, (GColor)Day_Colour);  
+  }else{
+    window_set_background_color(s_main_window, (GColor)Noon_Colour);  
+  }
+
+};
 
 //on shaking, the debug time will be shown for 5 seconds
 static void tap_handler(AccelAxisType axis, int32_t direction) {
@@ -77,145 +219,9 @@ static void graphics_update_proc(Layer *this_layer, GContext *ctx) {
   //TODO: find std GNU C lib and add to src
   //http://www.gnu.org/software/libc/download.html
   
-  
-  //update background colour
-  //1300 treated as peak of day, sunrise/sunset is roughly 0600-2100 in summer
-  
-  //TODO: change time diff to account for minutes as well so that daylight hours can be more accurately tracked
-  unsigned int time_diff = (unsigned int)(num_hour-13>=0 ? num_hour-13 : 13-num_hour);
-  if (time_diff >= 8)
-  {
-    window_set_background_color(s_main_window, (GColor)Night_Colour);   
-  }else if (time_diff >= 6)
-  {
-    window_set_background_color(s_main_window, (GColor)Dawn_Colour);   
-  }else if (time_diff >= 2)
-  {
-    window_set_background_color(s_main_window, (GColor)Day_Colour);  
-  }else{
-    window_set_background_color(s_main_window, (GColor)Noon_Colour);  
-  }
-  
-  
-  //Draw IC body
-  graphics_context_set_fill_color(ctx, (GColor)IC_Colour);
-  graphics_fill_rect(ctx, 
-					(GRect){.origin = (GPoint){.x = SCREEN_WIDTH/2-IC_WIDTH/2,.y = SCREEN_HEIGHT/2-IC_HEIGHT/2},
-								.size = (GSize){.w = IC_WIDTH,.h = IC_HEIGHT}
-						    },
-					IC_CORNER_RADIUS,
-					GCornersAll);
-  	
-  //draw seconds IC body horizontally on the bottom
-  graphics_context_set_fill_color(ctx, (GColor)IC_Colour);
-  //rectangle gets drawn past the bottom limit of the screen to prevent the bottom from rounding
-  graphics_fill_rect(ctx, 
-                     (GRect){.origin = (GPoint){.x = SCREEN_WIDTH/2-IC_HEIGHT/2,.y = SCREEN_HEIGHT-IC_WIDTH/8},
-                       .size = (GSize){.w = IC_HEIGHT,.h = IC_WIDTH/8+IC_CORNER_RADIUS}
-                            },
-                     IC_CORNER_RADIUS,
-                     GCornersAll);
-  
-  int right_pins = debug_hide_time > 0 ? num_day : num_min;
-  int left_pins = debug_hide_time > 0 ? num_month : num_hour;
-  //Draw IC pins and binary lights
-	for (int i = 0; i<6; i++)	{
-    int x_ = SCREEN_WIDTH/2-IC_WIDTH/2-PIN_WIDTH;
-    int y_ = SCREEN_HEIGHT/2-IC_HEIGHT/2+i*PIN_OFFSET;
-    
-    		//draw hour/month pins
-		if (i >0){
-			if (left_pins >> (5-i) & 1){
-        //TODO: figure out how to make this into the ternary:
-        //      graphics_context_set_fill_color(ctx, (GColor)(debug_hide_time == -1 ? Hour_Light_Colour:Month_Light_Colour));
-        if (debug_hide_time == -1)
-        {
-          graphics_context_set_fill_color(ctx, (GColor)Hour_Light_Colour);  
-        }
-        else{
-          graphics_context_set_fill_color(ctx, (GColor)Month_Light_Colour);  
-        }
-        }
-      else{
-        graphics_context_set_fill_color(ctx, (GColor)Pin_Colour);  
-      }
-      graphics_fill_rect(ctx, 
-                         (GRect){.origin = (GPoint){.x = x_,.y = y_},
-                           .size = (GSize){.w = PIN_WIDTH,.h = PIN_HEIGHT}
-                                },
-                         PIN_CORNER_RADIUS,
-                         GCornersAll);
-			
-		}
-		else{
-		  //draw MMDD pin
-			if (debug_hide_time > 0){
-        graphics_context_set_fill_color(ctx, (GColor)MMDD_Light_Colour);
-      }else{
-        graphics_context_set_fill_color(ctx, (GColor)Pin_Colour);
-      }
-      graphics_fill_rect(ctx, 
-                         (GRect){.origin = (GPoint){.x = x_,.y = y_},
-                           .size = (GSize){.w = PIN_WIDTH,.h = PIN_HEIGHT}
-                                },
-                         1,
-                         GCornersAll);
-		}
-		//draw minute/day pins
-		if (right_pins >> (5-i) & 1)
-		{
-      //TODO: figure out how to make this into the ternary:
-      // 		  graphics_context_set_fill_color(ctx, (GColor)(debug_hide_time == -1 ? Min_Light_Colour:Day_Light_Colour));
-      if (debug_hide_time == -1)
-      {
-        graphics_context_set_fill_color(ctx, (GColor)Min_Light_Colour);  
-      }
-      else{
-        graphics_context_set_fill_color(ctx, (GColor)Day_Light_Colour);  
-      }
-		  
-		}else{
-      graphics_context_set_fill_color(ctx, (GColor)Pin_Colour);
-    }
-		graphics_fill_rect(ctx, 
-							(GRect){.origin = (GPoint){.x = x_ + IC_WIDTH + PIN_WIDTH,.y = y_},
-									  .size = (GSize){.w = PIN_WIDTH,.h = PIN_HEIGHT}
-									},
-						  PIN_CORNER_RADIUS,
-						  GCornersAll);
-  //draw seconds pins
-  graphics_context_set_fill_color(ctx, (GColor)Pin_Colour);
-
-    x_ = SCREEN_WIDTH/2 - IC_HEIGHT/2 + PIN_OFFSET*i;
-    y_ = SCREEN_HEIGHT-IC_WIDTH/8-PIN_WIDTH*PIN_SCALE;
-
-    //draw second pins
-    if (num_second >> (5-i) & 1)
-    {
-      graphics_context_set_fill_color(ctx, (GColor)Second_Light_Colour);
-
-    }else{
-      graphics_context_set_fill_color(ctx, (GColor)Pin_Colour);
-    }
-    graphics_fill_rect(ctx, 
-                       (GRect){.origin = (GPoint){.x = x_,.y = y_},
-                         .size = (GSize){.w = PIN_HEIGHT,.h = PIN_WIDTH*PIN_SCALE}
-                              },
-                       PIN_CORNER_RADIUS,
-                       GCornersAll);
-
-  
-    
-	}// end for loop
-
-  
-	//TODO: if on debug time, show month/day
-	//! if the watch is updated to never show debug time, this criteria will need to be replaced by a flag
-	if (!layer_get_hidden((Layer*)s_time_layer)){
-	}
-      
-  
-	
+  update_background_color(this_layer, ctx);  
+  draw_time_IC(this_layer, ctx, SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
+  draw_seconds_IC(this_layer, ctx, SCREEN_WIDTH/2, SCREEN_HEIGHT+IC_WIDTH/4);
 }//end graphics_update_proc
 
 static void update_time() {
@@ -244,10 +250,10 @@ static void update_time() {
   if (!(num_hour || num_min || num_second)){
     static char month_string[] = "00";
     static char day_string[] = "00";
-	  strftime(month_string, sizeof("00"), "%m", tick_time);
+    strftime(month_string, sizeof("00"), "%m", tick_time);
     strftime(day_string, sizeof("00"), "%d", tick_time);
-	  num_month = int_from_string(month_string);
-	  num_day = int_from_string(day_string);
+    num_month = int_from_string(month_string);
+    num_day = int_from_string(day_string);
   }
   
     //hide the debug time string if it has been visible for 5 seconds
@@ -307,7 +313,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
       show_debug_time = t->value->uint32;  
       break;
     case KEY_SHOW_WEATHER:
-	  show_weather_ic = t->value->uint32;
+    show_weather_ic = t->value->uint32;
       break;
     case KEY_TEMPERATURE:
 
@@ -349,7 +355,7 @@ static void init() {
   //register for callbacks 
   app_message_register_inbox_received(inbox_received_callback);
   // Open AppMessage
-	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 
   
   //first time month and day
@@ -358,12 +364,12 @@ static void init() {
   struct tm *tick_time = localtime(&temp);
 
   //update month and day 
-	static char month_string[] = "00";
-	static char day_string[] = "00";
-	strftime(month_string, sizeof("00"), "%m", tick_time);
-	strftime(day_string, sizeof("00"), "%d", tick_time);
-	num_month = int_from_string(month_string);
-	num_day = int_from_string(day_string);
+  static char month_string[] = "00";
+  static char day_string[] = "00";
+  strftime(month_string, sizeof("00"), "%m", tick_time);
+  strftime(day_string, sizeof("00"), "%d", tick_time);
+  num_month = int_from_string(month_string);
+  num_day = int_from_string(day_string);
 }
 
 static void deinit() {
